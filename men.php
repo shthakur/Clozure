de6f0f0babd26c2ca45f2f1b1ca1cb2f0ba4c701
Shruti Thakur <thakur.shruti612@gmail.com>
2014-07-04 11:40:23 +0530

    again done changes
    :wq

---
+++ men.php
@@ -0,0 +1,104 @@
+<?php include("head.php") ?>
+<div id="1" style="position:absolute; top:500;left:100"><?php include("sidebar.html") ?>
+<div id="main">
+    <div class="cl">&nbsp;</div>
+    <!-- Content -->
+    <div id="content">
+      <!-- Content Slider -->
+    
+       
+ <div class="products">
+<div class="cl">&nbsp;</div>
+        <ul>
+          <li> <a href="#"><img src="css/images/5.jpg" alt="" /></a>
+            <div class="product-info">
+              <h3>LOREM IPSUM</h3>
+              <div class="product-desc">
+                <h4>WOMEN’S</h4>
+                <p>Lorem ipsum dolor sit<br />
+                  amet</p>
+                <strong class="price">$58.99</strong> </div>
+            </div>
+          </li>
+          <li> <a href="#"><img src="css/images/6.jpg" alt="" /></a>
+            <div class="product-info">
+              <h3>LOREM IPSUM</h3>
+              <div class="product-desc">
+                <h4>WOMEN’S</h4>
+                <p>Lorem ipsum dolor sit<br />
+                  amet</p>
+                <strong class="price">$58.99</strong> </div>
+            </div>
+          </li>
+         
+           <li> <a href="#"><img src="css/images/7.jpg" alt="" /></a>
+            <div class="product-info">
+              <h3>LOREM IPSUM</h3>
+              <div class="product-desc">
+                <h4>WOMEN’S</h4>
+                <p>Lorem ipsum dolor sit<br />
+                  amet</p>
+                <strong class="price">$58.99</strong> </div>
+            </div>
+          </li>
+          <li> <a href="#"><img src="css/images/8.jpg" alt="" /></a>
+            <div class="product-info">
+              <h3>LOREM IPSUM</h3>
+              <div class="product-desc">
+                <h4>WOMEN’S</h4>
+                <p>Lorem ipsum dolor sit<br />
+                  amet</p>
+                <strong class="price">$58.99</strong> </div>
+            </div>
+          </li>
+         
+        </ul>
+        <ul>
+        <li> <a href="#"><img src="css/images/5.jpg" alt="" /></a>
+            <div class="product-info">
+              <h3>LOREM IPSUM</h3>
+              <div class="product-desc">
+                <h4>WOMEN’S</h4>
+                <p>Lorem ipsum dolor sit<br />
+                  amet</p>
+                <strong class="price">$58.99</strong> </div>
+            </div>
+          </li>
+          <li> <a href="#"><img src="css/images/6.jpg" alt="" /></a>
+            <div class="product-info">
+              <h3>LOREM IPSUM</h3>
+              <div class="product-desc">
+                <h4>WOMEN’S</h4>
+                <p>Lorem ipsum dolor sit<br />
+                  amet</p>
+                <strong class="price">$58.99</strong> </div>
+            </div>
+          </li>
+         
+           <li> <a href="#"><img src="css/images/7.jpg" alt="" /></a>
+            <div class="product-info">
+              <h3>LOREM IPSUM</h3>
+              <div class="product-desc">
+                <h4>WOMEN’S</h4>
+                <p>Lorem ipsum dolor sit<br />
+                  amet</p>
+                <strong class="price">$58.99</strong> </div>
+            </div>
+          </li>
+          <li> <a href="#"><img src="css/images/8.jpg" alt="" /></a>
+            <div class="product-info">
+              <h3>LOREM IPSUM</h3>
+              <div class="product-desc">
+                <h4>WOMEN’S</h4>
+                <p>Lorem ipsum dolor sit<br />
+                  amet</p>
+                <strong class="price">$58.99</strong> </div>
+            </div>
+          </li>
+         
+        </ul>
+        <div class="cl">&nbsp;</div>
+      </div></div></div>
+      <!-- End Products -->
+      <?php include("footer.html") ?>
+  </div></body></html>
